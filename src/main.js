94f04a8657c73fee49cf7e0c6c63317c53a340a3
pmengy <936841379@qq.com>
2022-07-06 09:00:02 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 // 引入第三方包里的bootstrap 的 css文件
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import 'bootstrap/dist/css/bootstrap.css';
 
 // 目标: 请求数据 - 打印
 // 1. 下载axios库, main.js - 全局绑定属性 (确保任意.vue文件可以都访问到这个axios方法)
@@ -11,7 +12,6 @@ Vue.config.productionTip = false
 
 // 3. axios方法添加到Vue的原型上
 
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
